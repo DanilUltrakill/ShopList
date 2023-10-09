bf3637f6fb08c1739b69c1b064047fb858868ed2
DanilUltrakill <yaremakadanil@gmail.com>
2023-10-09 18:55:52 +0700

    data done

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,4 +1,38 @@
 package com.example.shoppinglist.data
 
-class ShopListRepositoryImpl {
+import android.widget.Toast
+import com.example.shoppinglist.domain.ShopListRepository
+import com.example.shoppinglist.domain.entity.ShopItem
+
+object ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID)
+            shopItem.id = autoIncrementId++
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element not found (id: $shopItemId)")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
 }
